847c3aa9b21bc6763b0eaddeb186568e96309e63
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-03-02 11:31:15 -0500

    show properties of a component

--- app/app.component.js
+++ app/app.component.js
@@ -1,38 +1,52 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(["angular2/core"], function (exports_1, context_1) {
     "use strict";
-    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
         else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
         return c > 3 && r && Object.defineProperty(target, key, r), r;
     };
-    var __metadata = (this && this.__metadata) || function (k, v) {
-        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-    };
-    var core_1;
-    var AppComponent;
+    var __moduleName = context_1 && context_1.id;
+    var core_1, AppComponent;
     return {
-        setters:[
+        setters: [
             function (core_1_1) {
                 core_1 = core_1_1;
-            }],
-        execute: function() {
-            // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
+            }
+        ],
+        execute: function () {
+            /*
+            * exportamos la clase para que el componente este disponible
+            */
             AppComponent = (function () {
+                /*
+                * decorador component indicamos el selector donde cargar la
+                * plantilla y definimos el html de la plantilla. Utilizamos las
+                * dobles llaves para mostrar el valor de las propiedades o
+                * atributos de la clase AppComponent
+                */
                 function AppComponent() {
+                    /*
+                    * definimos las propiedades y su tipo
+                    */
+                    this.title = "Peliculas con Angular 2";
+                    this.movie = "Batman v Superman";
+                    this.director = "Zack Snider";
+                    this.year = "2016";
                 }
-                AppComponent = __decorate([
-                    core_1.Component({
-                        selector: 'my-app',
-                        template: '<h1>Hola mundo con Angular 2 !! jonnyalexbh</h1>'
-                    }), 
-                    __metadata('design:paramtypes', [])
-                ], AppComponent);
                 return AppComponent;
             }());
+            AppComponent = __decorate([
+                core_1.Component({
+                    selector: 'my-app',
+                    template: "<h1>{{title}}</h1>\n  <ul>\n  <li><b>movie: </b> {{movie}}</li>\n  <li><b>director: </b> {{director}}</li>\n  <li><b>year: </b> {{year}}</li>\n  </ul>"
+                })
+                /*
+                * exportamos la clase para que el componente este disponible
+                */
+            ], AppComponent);
             exports_1("AppComponent", AppComponent);
         }
-    }
+    };
 });
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,31 @@
-// Importar el núcleo de Angular
+/*
+* importar el núcleo de Angular
+*/
 import {Component} from 'angular2/core';
-
-// Decorador component, indicamos en que etiqueta se va a cargar la plantilla
+/*
+* decorador component indicamos el selector donde cargar la
+* plantilla y definimos el html de la plantilla. Utilizamos las
+* dobles llaves para mostrar el valor de las propiedades o
+* atributos de la clase AppComponent
+*/
 @Component({
   selector: 'my-app',
-  template: '<h1>Hola mundo con Angular 2 !! jonnyalexbh</h1>'
+  template: `<h1>{{title}}</h1>
+  <ul>
+  <li><b>movie: </b> {{movie}}</li>
+  <li><b>director: </b> {{director}}</li>
+  <li><b>year: </b> {{year}}</li>
+  </ul>`
 })
-
-// Clase del componente donde iran los datos y funcionalidades
-export class AppComponent { }
+/*
+* exportamos la clase para que el componente este disponible
+*/
+export class AppComponent {
+  /*
+  * definimos las propiedades y su tipo
+  */
+  public title = "Peliculas con Angular 2";
+  movie = "Batman v Superman";
+  public director = "Zack Snider";
+  public year = "2016";
+}
